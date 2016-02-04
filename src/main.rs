d3cd9f19612ac2ce6c8e0affe79543c51166b701
Sebastian Bensusan <sbensu@gmail.com>
2016-02-04 03:49:51 +0100

    WIP: implement show_sets

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,59 @@
 extern crate rand;
-
 use rand::random;
+
+mod union_find {
+    /* Implements disjoints sets through Union Find
+     * Each index of the sets array maps onto an object in the graph.
+     * The user can specify if two objects (a & b) are connected by
+     * stating `union(&mut sets,a,b)` which will mutate the sets to
+     * take the union into account.
+     * The user can check if two elements are conencted with
+     * connected(&sets, a, b)` and get a boolean.
+     */
+
+    /* The underlying data structure (sets) tracks the connected
+     * elements by keeping their values equal. If two indexes have the
+     * same value, they are connected. If two indexes need to be
+     * connected, one of them (and all it's associated indexes) change
+     * value to the others
+     */
+    pub fn connected(sets: &[usize], from: usize, to: usize) -> bool {
+        // Checks if two elements are connected.
+        sets[from] == sets[to]
+    }
+
+    pub fn union(sets: &mut [usize], from: usize, to: usize) {
+        // Joins two elements
+        let from_root = sets[from];
+        let to_root = sets[to];
+        // change all that have from_root to to_root
+        for i in 0..sets.len() {
+            if sets[i] == from_root {
+                sets[i] = to_root;
+            }
+        }
+    }
+
+    pub fn show_sets(sets: &[usize]) {
+        // Prints the grouped sets
+        let mut idxs: Vec<Vec<usize>> = Vec::new();
+        for i in 0..sets.len() {
+            idxs.push(Vec::new());
+        }
+
+
+        for (i,val) in sets.iter().enumerate() {
+            match idxs.get(*val) {
+                Some(ref xs) => (*xs).push(i),
+                None => (),
+            }
+        }
+        println!("{:?}", idxs);
+    }
+}
 
 fn random_upto(n: usize) -> usize {
     random::<usize>() % n
-}
-
-fn connected(sets: &[usize], from: usize, to: usize) -> bool {
-    // Checks if two elements are connected.
-    sets[from] == sets[to]
-}
-
-fn union(sets: &mut [usize], from: usize, to: usize) {
-    // Joins two elements
-    let from_root = sets[from];
-    let to_root = sets[to];
-    // change all that have from_root to to_root
-    for i in 0..sets.len() {
-        if sets[i] == from_root {
-            sets[i] = to_root;
-        }
-    }
 }
 
 fn main() {
@@ -41,15 +74,17 @@ fn main() {
     // logic for Union Find
     for i in 0..UNIONS {
         let (from,to) = unions[i];
-        union(&mut sets, from, to);
+        union_find::union(&mut sets, from, to);
     }
+
+    println!("The unions are: {:?}", unions);
+    println!("The sets are:");
+    union_find::show_sets(&sets);
 
     // test
     for i in 0..UNIONS {
         let (from,to) = unions[i];
-        println!("{}", connected(&sets, from, to));
+        assert!(union_find::connected(&sets, from, to));
     }
 
-    println!("{:?}", unions);
-    println!("{:?}", sets);
 }
